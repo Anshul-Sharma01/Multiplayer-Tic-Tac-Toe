897a99bbfb45455cbb6c7f393a07f7b114e2f9f9
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-09-03 11:24:16 +0530

    Added tic-tac-toe Visual structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
+import Square from './components/Square'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ squares, setSquares ] = useState([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ])
 
   return (
-    <>
-      
-    </>
+    <section className='flex justify-center items-center h-screen gap-20 flex-col'>
+      <div className='flex justify-between items-center gap-40'>
+        <div className='bg-orange-200 rounded-bl-3xl rounded-tr-3xl px-4 py-2'>
+            Opponent
+        </div>
+        <div className='bg-red-200 px-4 py-2 rounded-bl-3xl rounded-tr-3xl'>
+            You
+        </div>
+      </div>
+      <h1 className='bg-slate-400 px-4 py-2 text-3xl font-mono rounded-md uppercase tracking-wider'> Tic-Tac-Toe </h1>
+      <div className='w-fit grid gap-2 grid-cols-3'>
+        {
+          squares.map((ele) =>
+            ele.map((el) => <Square key={el}/>)
+          )
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ client/src/components/Square.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Square(){
+    return(
+        <>
+            <div className="bg-slate-500 hover:cursor-pointer border-solid border-2 border-white w-[120px] h-[120px] rounded-lg">
+
+            </div>
+        </>
+    )
+}
+
+export default Square;
