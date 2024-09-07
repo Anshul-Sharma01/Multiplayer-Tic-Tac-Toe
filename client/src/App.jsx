021dd3b3afd374978e62d0311557876ef5ba2d0b
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-09-07 18:43:58 +0530

    Implemented State updation on clicking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,15 @@ import Square from './components/Square'
 
 function App() {
 
-  const [ squares, setSquares ] = useState([
+  const [ gameState, setGameState ] = useState([
     [1,2,3],
     [4,5,6],
     [7,8,9]
   ])
+
+  const [ currPlayer, setCurrPlayer ] = useState('circle');
+  const [ finishedState, setFinishedState ] = useState(false);
+
 
   return (
     <section className='flex justify-center items-center h-screen gap-20 flex-col'>
@@ -22,8 +26,16 @@ function App() {
       <h1 className='bg-slate-400 px-4 py-2 text-3xl font-mono rounded-md uppercase tracking-wider'> Tic-Tac-Toe </h1>
       <div className='w-fit grid gap-2 grid-cols-3'>
         {
-          squares.map((ele) =>
-            ele.map((el) => <Square key={el}/>)
+          gameState.map((ele, rowIndex) =>
+            ele.map((el, colIndex) => <Square
+            finishedState={finishedState}
+            setFinishedState={setFinishedState}
+            id={rowIndex * 3 + colIndex}
+            setGameState = {setGameState} 
+            currPlayer={currPlayer} 
+            setCurrPlayer={setCurrPlayer}  
+            key={rowIndex * 3 + colIndex}/>
+          )
           )
         }
       </div>
--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRegCircle } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 
+function Square({ id, currPlayer, setCurrPlayer, setGameState, finishedState, setFinishedState }){
 
-function Square(){
+    const [icon, setIcon] = useState(null);
+
+    function updateSquareIcon(){
+        if(!icon ){
+            if(currPlayer === 'circle'){
+                setIcon(<FaRegCircle/>);
+            }else{
+                setIcon(<ImCross/>);
+            }
+
+            const myCurrPlayer = currPlayer;
+
+            setCurrPlayer ( currPlayer === 'circle' ? "cross" : "circle");
+            setGameState(prev => {
+                let newState = [...prev];
+                const rowIndex = Math.floor(id / 3);
+                const colIndex = id % 3;
+                newState[rowIndex][colIndex] = myCurrPlayer
+
+                console.log(prev);
+                return newState;
+            })
+        }
+
+    }
+
     return(
         <>
-            <div className="bg-slate-500 hover:cursor-pointer border-solid border-2 border-white w-[120px] h-[120px] rounded-lg">
-
+            <div onClick={updateSquareIcon}  className="text-white  text-5xl  flex justify-center items-center bg-slate-500 hover:cursor-pointer border-solid border-2 border-white w-[120px] h-[120px] rounded-lg">
+                {icon}
             </div>
         </>
     )
